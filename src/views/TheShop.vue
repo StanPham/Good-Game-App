7d2647a56c905fe7394e9c074a0a9168366f5c5b
WinnerxSinclair <cobrasuperkek@gmail.com>
2023-10-03 18:06:49 -0500

    added a ton of shiiiiiiiiiiiiiiiiit

--- src/views/TheShop.vue
+++ src/views/TheShop.vue
@@ -1,37 +1,120 @@
 <template>
+    <div class="body-wrapper">
+  
+
+    <div class="search">
+        <img src="../components/search.svg" alt="deez" class="search-icon">
+        <input type = "text" class="input-box" placeholder="Search">
+    </div>
+    <div class="cat-wrap">
+    <label for="product-select"></label>
+
+    <select name="products" id="product-select" v-model="selectedCategory">
+    <option value="">--Please choose a category--</option>
+    <option value="mtg">Magic: The Gathering</option>
+    <option value="yug">Yugioh</option>
+    <option value="pok">Pokemon</option>
+    <option value="ddd">D&D</option>
+    <option value="oth">Accessories</option>
     
-    <div class="header"></div>
-    <main class="product-container">
+    </select>
+    </div>
+    <!-- <div class="filter">
+        <img src="../components/filter.svg" alt="deez" class="filter-icon">
+        <button class="filter-button">Filter By</button>
+    </div> -->
+
+
+    <main class="product-container" >
+        <div class="work-wrap" v-for="shop in filteredShops" :key="shop.id">
         <div class="prac">
-            <img src="../components/pikachu.webp" alt="" class="pika">
+            <img :src="shop.image" alt="" class="pika">
+            <div class="product-name">{{shop.name}}</div>
+            <div class="price">{{ shop.price }}</div>
+            <p class="quant">Quantity: {{shop.quant}}</p>
+            <br>
         </div>
-        <div class="prac">
-            <img src="../components/pikachu.webp" alt="" class="pika">
         </div>
-        <div class="prac">
-            <img src="../components/pikachu.webp" alt="" class="pika">
-        </div>
-        <div class="prac">
-            <img src="../components/pikachu.webp" alt="" class="pika">
-        </div>
-        <div class="prac">
-            <img src="../components/pikachu.webp" alt="" class="pika">
-        </div>
-        <div class="prac">
-            <img src="../components/pikachu.webp" alt="" class="pika">
-        </div>
-        <div class="prac">
-            <img src="../components/pikachu.webp" alt="" class="pika">
-        </div>
-        <div class="prac">
-            <img src="../components/pikachu.webp" alt="" class="pika">
-        </div>
+        
     </main>
+</div>
     
 </template>
 
 <style scoped>
+.search-icon{
+    width:20px;
+    position:absolute;
+    top: 45%;
+    left: 15%;
+   
+}
+.filter-icon{
+    width:20px;
+    margin-left:10px;
+}
+.filter{
+   
+    margin-top:1rem;
+    margin-left:1rem;
+    display:flex;
+    width:clamp(1px,40%,15rem);
+    border-radius: 1rem;
+    background-color: white;
+    border:black 1px solid;
+}
+.filter-button{
+    
+   
+    
+    color:black;
+    padding: 2px 1rem;
+  
+   position:relative;
+   
+}
 
+.input-box{
+    width:clamp(20rem, 50%,90vw);
+    background-color: rgb(238, 235, 235);
+    border-radius: 20px;
+    border: 1px solid #21272b;
+    padding-left:50px;
+    max-height: 30px;
+    height:30px ;
+    color:black;
+   
+}
+.search{
+  width:100vw;
+  padding-top:1rem;
+  display:flex;
+  justify-content: center;
+  position:relative;
+ 
+   
+}
+
+select{
+    background-color: rgb(0, 0, 0);
+    font-size:1.5rem;
+   
+}
+
+.cat-wrap{
+   display:flex;
+   justify-content: center;
+   padding-top:1rem;
+}
+.body-wrapper{
+    margin-top:3rem;
+}
+.quant{
+    font-size: .8rem;
+}
+.price{
+    font-weight: bold;
+}
 .header{
     background-color: grey;
     width:100%;
@@ -42,17 +125,15 @@ main{
     grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
    
     
-    
-    gap: 2rem;
+    margin:1rem;
+    gap: 1.3rem;
     max-width:100%;
 
 
 }
 .prac{
-    
-   
-   
-    background-color: red;
+    padding-left:1rem;
+    background-color: rgb(87, 38, 38);
     
 }
 img{
@@ -60,3 +141,52 @@ img{
 }
 
 </style>
+
+<script setup>
+import {ref, computed} from 'vue';
+import pika from '../components/pikachu.webp'
+const selectedCategory = ref("");
+const myShops = ref([
+    {
+        id:1,
+        image: pika,
+        name: 'Wilds of Eldraine Set Boosters',
+        price: '$99.99',
+        quant: 3,
+        category: 'MTG',
+
+    },
+    {
+        id:2,
+        image: pika,
+        name: 'Wilds of Eldraine Set Boosters',
+        price: '$99.99',
+        quant: 3,
+        category: 'Pokemon',
+
+    },
+    {
+        id:3,
+        image: pika,
+        name: 'Wilds of Eldraine Set Boosters',
+        price: '$99.99',
+        quant: 3,
+        category: 'Yugioh',
+
+    },
+]);
+const categoryMapping = {
+    mtg: 'MTG',
+    yug: 'Yugioh',
+    pok: 'Pokemon',
+    ddd: 'D&D',
+    oth: 'Accessories'
+};
+const filteredShops = computed(() => {
+    if (!selectedCategory.value) return myShops.value;  
+    
+    const actualCategory = categoryMapping[selectedCategory.value];
+    return myShops.value.filter(shop => shop.category === actualCategory);
+});
+
+</script>
