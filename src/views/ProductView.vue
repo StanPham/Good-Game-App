6c9c76dd8f8faf4b24b386af0991deca8f4518cf
WinnerxSinclair <cobrasuperkek@gmail.com>
2023-10-16 20:32:32 -0500

    product

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,18 +1,3 @@
-<template>
-    <div class="deez" style="margin-top:20rem">
-<div v-if="productData">
-    <h1>{{ productData.name }}</h1>
-    <img :src="productData.img" alt="Product Image">
-    <p>{{ productData.desc }}</p>
-    <p>Price: {{ productData.price }}</p>
-    <p>Quantity: {{ productData.quant }}</p>
-</div>
-<div v-else>
-    Loading...
-</div>
-</div>
-</template>
-
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
@@ -34,3 +19,84 @@ onMounted(async () => {
   }
 });
 </script>
+
+
+<template>
+<div class="main-contain">
+  
+  
+  <div class="product-container" v-if="productData">
+      <img :src="productData.img" alt="Product Image" class="product-img">
+    <div class="product-text">
+      <h1>{{ productData.name }}</h1>
+      <p>{{ productData.desc }}</p>
+      <p class="price">{{ productData.price }}</p>
+      <br>
+      <div class="quantity-wrap">
+        <select>
+          <option>1</option>
+        </select>
+        <button class="reserve-btn">Reserve Now</button>
+      </div>
+    </div>
+  </div>
+
+  <div v-else>
+    Loading...
+  </div>
+</div>
+</template>
+
+<style scoped>
+select{
+  width:20%;
+  border-radius:1rem;
+  background-color: rgb(0, 0, 0);
+  font-size:1.5rem;
+  text-align: center;
+}
+.reserve-btn{
+  font-size:1.5rem;
+  background:var(--btn-color);
+  padding:1rem;
+  border-radius:1rem;
+}
+.quantity-wrap{
+  display:flex;
+  padding-bottom:1rem;
+  gap:1rem;
+}
+.price{
+  font-weight:bold;
+  padding-top:1rem;
+  font-size:1.8rem;
+}
+p{
+  font-size:1.4rem;
+}
+.main-contain{
+  margin-top:3rem;
+ 
+  
+}
+
+.product-container{
+  display:flex;
+  justify-content: center;
+  align-items:center;
+ flex-wrap:wrap;
+ background:black;
+ gap:2rem;
+
+ 
+}
+
+.product-img{
+  width:clamp(500px, 50%, 50rem);
+  height: clamp(500px, 50%, 50rem);
+}
+
+.product-text{
+  text-align:left;
+}
+</style>
