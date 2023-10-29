16eea080b3e05d598160af029ca87252b1310f49
WinnerxSinclair <cobrasuperkek@gmail.com>
2023-10-29 13:52:59 -0500

    crude modal

---
+++ src/components/modals/PhoneNumber.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+const phone = ref('');
+</script>
+
+
+<template>
+  <div class="form-container">
+    <form @submit="">
+      <div class="form-header">
+        <h2>Phone Number Verification Required</h2>
+      </div>
+      <label for="phone">Enter Phone Number</label>
+      <input type="tel" v-model="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+@import '@/assets/LoginSignup.css';
+
+form{
+  width:auto;
+}
+</style>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -4,11 +4,13 @@ import { useRoute } from 'vue-router';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '@/firebase';
 import router from '../router'
+import PhoneNumber from '../components/modals/PhoneNumber.vue';
 
 const amount = ref('1');
 const route = useRoute();
 const productID = route.params.name; 
 const productData = ref(null);
+let verifiedPhoneNumber = ref(false);
 
 onMounted(async () => {
   const productDoc = doc(db, 'shop', productID); 
@@ -27,11 +29,21 @@ const productQuantityOptions = computed(() => {
 
 const goHome = () => router.push('/');
 const goShop = () => router.push('/shop');
+
+let phoneModal = () => verifiedPhoneNumber.value = true;
 </script>
 
 <template>
+  <PhoneNumber v-if="verifiedPhoneNumber"/>
   <div class="view-top-margin flex-c-c">
     <div class="product-container flex-c-c wrap rc rel" v-if="productData">
+      <nav class="breadcrumbs flex pad abs">
+        <a class="underline" @click="goHome">Home</a>
+        <div>></div>
+        <a class="underline" @click="goShop">Shop</a>
+        <div>></div>
+        <a>{{ productData.name }}</a>
+      </nav>
       <img :src="productData.img" alt="Product Image" class="product-img">
       <div class="product-text">
         <h1>{{ productData.name }}</h1>
@@ -44,17 +56,11 @@ const goShop = () => router.push('/shop');
               {{ num }}
             </option>
           </select>
-          <button class="btn-even-pad">Reserve Now</button>
+          <button class="btn-even-pad" @click="phoneModal">Reserve Now</button>
         </div>
         <div v-else class="title-scale" style="font-style:italic">Sold Out</div>
       </div>
-      <nav class="breadcrumbs flex pad abs">
-        <a class="underline" @click="goHome">Home</a>
-        <div>></div>
-        <a class="underline" @click="goShop">Shop</a>
-        <div>></div>
-        <a>{{ productData.name }}</a>
-      </nav>
+      
     </div>
     
     <div v-else>
@@ -90,11 +96,13 @@ p{
   background:black;
   gap:2rem;
   width:clamp(350px, 80rem, 100%);
+  padding-bottom:2rem;
 }
 
 .product-img{
-  width:clamp(500px, 50%, 50rem);
-  height: clamp(50%, 20vh, 40rem);
+  width:clamp(250px,500px,90%);
+  height:450px;
+  margin-top:4rem;
 }
 
 .product-text{
@@ -102,5 +110,14 @@ p{
   min-width:22rem;
   max-width:25rem;
   padding:.5rem;
+}
+
+@media(max-width:1000px){
+  .breadcrumbs{
+    position:static;
+  }
+  .product-img{
+    margin-top:0;
+  }
 }
 </style>
