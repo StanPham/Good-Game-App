ae4a76af2388d7bc5c63ba4019b982b5c36a5fed
WinnerxSinclair <cobrasuperkek@gmail.com>
2023-10-31 16:08:54 -0500

    centered?

--- src/components/FacebookCard.vue
+++ src/components/FacebookCard.vue
@@ -1,62 +1,41 @@
-<script setup>
-import { onMounted } from 'vue';
-
-onMounted(() => {
-  // Ensure the SDK is loaded
-  if (!document.getElementById('facebook-jssdk')) {
-    // Create a script element for the Facebook SDK
-    const script = document.createElement('script');
-    script.id = 'facebook-jssdk';
-    script.async = true;
-    script.defer = true;
-    script.crossorigin = 'anonymous';
-    script.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v18.0";
-    script.nonce = "hBK6wuqC";
-    document.body.appendChild(script);
-
-    // Re-parse the Facebook content after the SDK loads
-    script.onload = () => {
-      if (window.FB) {
-        FB.XFBML.parse();
-      }
-    };
-  } else {
-    // If SDK was already loaded, re-parse the component for Facebook widgets
-    if (window.FB) {
-      FB.XFBML.parse();
-    }
-  }
-});
-</script>
-
-
-
 <template>
-  <div class="container flex-c-c">
-    <!-- Facebook Page Plugin -->
-    <div id="fb-root"></div>
-    <div class="fb-page" 
-         data-href="https://www.facebook.com/GoodGameRsvlAR/" 
-         data-tabs="timeline" 
-         data-width="500" 
-         data-height="300" 
-         data-small-header="true" 
-         data-adapt-container-width="true" 
-         data-hide-cover="false" 
-         data-show-facepile="true">
-      <blockquote cite="https://www.facebook.com/GoodGameRsvlAR/" 
-                  class="fb-xfbml-parse-ignore">
+  <div id="fb-root"></div>
+  <div class="xd-container rel">
+    <div class="fb-page" data-href="https://www.facebook.com/GoodGameRsvlAR/" data-tabs="timeline" data-width="500" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+      <blockquote cite="https://www.facebook.com/GoodGameRsvlAR/" class="fb-xfbml-parse-ignore">
         <a href="https://www.facebook.com/GoodGameRsvlAR/">Good Game</a>
       </blockquote>
     </div>
   </div>
 </template>
 
-  <style scoped>
-  .container{
-      background:black;
-      width:100%;
-      height:100%;
-      border-radius:1rem;
-  }
-  </style>
+<script>
+export default {
+  name: 'YourComponentName',
+  mounted() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.defer = true;
+    script.crossorigin = 'anonymous';
+    script.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v18.0';
+    script.nonce = 'hBK6wuqC';
+    document.body.appendChild(script);
+  },
+};
+</script>
+
+<style scoped>
+.xd-container {
+  background: black;
+  width: 100%;
+  height: 100%;
+  border-radius: 1rem;
+}
+.fb_iframe_widget{
+  position:absolute;
+  display:block;
+  top:50%;
+  left:50%;
+  transform:translate(-50%, -50%);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@ import TodaysEvent from '../components/TodaysEvent.vue';
 import ShopCard from '../components/MobileShopCard.vue';
 import ShopComp from '../components/DtopShopComp.vue';
 import TableCard from '../components/TableCard.vue'
-import MiniGame from '../components/MiniGame.vue';
+import FacebookCard from '../components/FacebookCard.vue';
 
 
 const mySlides = ref([]);
@@ -124,7 +124,7 @@ const myShops = computed(() => {
     </div>
 
     <div class="minigame-card">
-      <MiniGame />
+      <FacebookCard />
     </div>
   </div>
 </template>
