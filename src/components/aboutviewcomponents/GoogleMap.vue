e28965ad999a33a322743377977d09ff16fa741b
WinnerxSinclair <cobrasuperkek@gmail.com>
2023-11-01 14:39:33 -0500

    map popup

--- src/components/aboutviewcomponents/GoogleMap.vue
+++ src/components/aboutviewcomponents/GoogleMap.vue
@@ -1,33 +1,40 @@
 <template>
-    <GMapMap
-        :center="{lat: 35.282609, lng: -93.134994}"
-        :zoom="20"
-        map-type-id="terrain"
-        style="width: 100%;height: 50vh; border-radius: 0 0 1rem 1rem;"
-    >
+  <GMapMap
+    :center="{lat: 35.282609, lng: -93.134994}"
+    :zoom="20"
+    map-type-id="terrain"
+    style="width: 100%;height: 50vh; border-radius: 0 0 1rem 1rem;"
+  >
     <GMapMarker
-    :key="index"
-  v-for="(m, index) in markers"
-  :position="m.position"
-    />
-    </GMapMap>
-  </template>
-  
-  <script >
-  export default {
-    name: 'App',
-    data() {
-      return {
-        center: {lat: 35.282609, lng: -93.134994},
-        markers: [
-        {
-          position: {
-            lat: 35.282565, lng: -93.134994
-          },
-        }
-      ]
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+    >
+      <GMapInfoWindow >
+        <div style="color: black;">
+        407 N Arkansas Ave #13
+        Russellville, AR 72801
+        </div>
+      </GMapInfoWindow>
+    </GMapMarker>
+  </GMapMap>
+</template>
+          
+
+<script >
+export default {
+  data() {
+    return {
+      center: {lat: 35.282609, lng: -93.134994},
+      markers: [
+      {
+        position: {
+          lat: 35.282565, lng: -93.134994
+        },
       }
+    ]
     }
   }
-  </script>
+}
+</script>
   
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,52 +1,12 @@
 <script setup>
 import GoogleMapVue from '../components/aboutviewcomponents/GoogleMap.vue';
+import TheHours from '../components/aboutviewcomponents/TheHours.vue'
 </script>
 
 <template>
   <div class="flex-c gap main-container">
     <div class="hours-container black rc">
-      <h1 class="reverse-gradient pad rc-top-top cool-font text-center">Hours & Contact</h1>
-      <article class="pad">
-        <div class="contact-container pad-bot">
-          <div class="flex">
-            <div class="title grow">479-219-5162</div>
-            <div class="flex gap">
-              <a href="https://discord.com/invite/AmCadcysE4">
-                <img src="../images/discord.svg" alt="" class="icon-white icon-small">
-              </a>
-              <a href="https://www.facebook.com/GoodGameRsvlAR/">
-                <img src="../images/facebook.svg" alt="" class="icon-white icon-small">
-              </a>
-              <a href="https://www.instagram.com/goodgamersvl">
-                <img src="../images/instagram.svg" alt="" class="icon-white icon-small">
-              </a>
-              
-            </div>
-          </div>
-          <a href="mailto:goodgame.rsvl.ar@gmail.com" class="title link-hover">goodgame.rsvl.ar@gmail.com</a>
-        </div>
-        <hr>
-        <div class="pad-top flex gap-big">
-          <ul class="flex column gap title jockey days">
-            <li class="days">Monday</li>
-            <li class="days">Tuesdays</li>
-            <li class="days">Wednesdays</li>
-            <li class="days">Thursdays</li>
-            <li class="days">Fridays</li>
-            <li class="days">Saturdays</li>
-            <li class="days">Sundays</li>
-          </ul>
-          <ul class="hours-list flex column gap title">
-            <li class="hours">Closed</li>
-            <li class="hours">Closed</li>
-            <li class="hours">2-10PM</li>
-            <li class="hours">2-10PM</li>
-            <li class="hours">2-10PM</li>
-            <li class="hours">12-10PM</li>
-            <li class="hours">12-6PM</li>
-          </ul>
-        </div>
-      </article> 
+      <TheHours />
     </div>
 
     <div class="google-maps-container  rc black text-center">
@@ -57,28 +17,19 @@ import GoogleMapVue from '../components/aboutviewcomponents/GoogleMap.vue';
 </template>
 
 <style scoped>
-.hours-list{
-  padding-top:2px;
-}
-ul{
-  list-style-type:none;
-}
 
-.days{
-  text-align:left;
-}
 .main-container{
   width:90%;
   margin-left:auto;
   margin-right:auto;
   margin-top:calc(var(--header-height) + 3rem);
 }
-  .hours-container{
-    width:clamp(350px, 35%, 35rem);
-    
-  }
-  .google-maps-container{
-    width:clamp(350px, 60%, 40rem);
-    
-  }
+.hours-container{
+  width:clamp(350px, 35%, 35rem);
+  
+}
+.google-maps-container{
+  width:clamp(350px, 60%, 40rem);
+  
+}
 </style>
