ccdf65fd86b5d55aeccfaf856ed83e2cb4ca9584
WinnerxSinclair <cobrasuperkek@gmail.com>
2023-10-31 20:07:02 -0500

    added map marker

--- src/components/aboutviewcomponents/GoogleMap.vue
+++ src/components/aboutviewcomponents/GoogleMap.vue
@@ -5,6 +5,11 @@
         map-type-id="terrain"
         style="width: 100vw; height: 900px"
     >
+    <GMapMarker
+    :key="index"
+  v-for="(m, index) in markers"
+  :position="m.position"
+    />
     </GMapMap>
   </template>
   
@@ -14,6 +19,13 @@
     data() {
       return {
         center: {lat: 35.282609, lng: -93.134994},
+        markers: [
+        {
+          position: {
+            lat: 35.282565, lng: -93.134994
+          },
+        }
+      ]
       }
     }
   }
