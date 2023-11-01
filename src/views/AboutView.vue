cc6a81b1fa3584b7d4cb4e09519d7913d6123d3c
WinnerxSinclair <cobrasuperkek@gmail.com>
2023-11-01 10:34:29 -0500

    styles

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,17 +4,42 @@ import GoogleMapVue from '../components/aboutviewcomponents/GoogleMap.vue';
 
 <template>
   <div class="flex-c gap main-container">
-    <div class="hours-container text-center black rc">
-      <h1 class="reverse-gradient pad rc-top-top cool-font">Hours</h1>
-      <ul class="flex column gap title test">
-        <li>Monday: Closed</li>
-        <li>Tuesday: Closed</li>
-        <li>Wednesday: 2-10 PM</li>
-        <li>Thursday: 2-10 PM</li>
-        <li>Friday: 2-10 PM</li>
-        <li>Saturday: 12-10 PM</li>
-        <li>Sunday: 12-6 PM</li>
-      </ul>
+    <div class="hours-container black rc">
+      <h1 class="reverse-gradient pad rc-top-top cool-font text-center">Hours & Contact</h1>
+      <article class="pad">
+        <div class="contact-container pad-bot">
+          <div class="flex">
+            <div class="title grow">479-219-5162</div>
+            <div class="flex gap">
+              <img src="../images/discord.svg" alt="" class="icon-white icon-small">
+              <img src="../images/facebook.svg" alt="" class="icon-white icon-small">
+              <img src="../images/instagram.svg" alt="" class="icon-white icon-small">
+            </div>
+          </div>
+          <div class="title">goodgame.rsvl.ar@gmail.com</div>
+        </div>
+        <hr>
+        <div class="pad-top flex gap-big">
+          <ul class="flex column gap title jockey days">
+            <li class="days">Monday</li>
+            <li class="days">Tuesdays</li>
+            <li class="days">Wednesdays</li>
+            <li class="days">Thursdays</li>
+            <li class="days">Fridays</li>
+            <li class="days">Saturdays</li>
+            <li class="days">Sundays</li>
+          </ul>
+          <ul class="hours-list flex column gap title">
+            <li class="hours">Closed</li>
+            <li class="hours">Closed</li>
+            <li class="hours">2-10PM</li>
+            <li class="hours">2-10PM</li>
+            <li class="hours">2-10PM</li>
+            <li class="hours">12-10PM</li>
+            <li class="hours">12-6PM</li>
+          </ul>
+        </div>
+      </article> 
     </div>
     <div class="google-maps-container  rc black text-center">
       <h1 class="gradient pad rc-top-top cool-font">Our Location</h1>
@@ -24,7 +49,14 @@ import GoogleMapVue from '../components/aboutviewcomponents/GoogleMap.vue';
 </template>
 
 <style scoped>
-.test{
+.hours-list{
+  padding-top:2px;
+}
+ul{
+  list-style-type:none;
+}
+
+.days{
   text-align:left;
 }
 .main-container{
