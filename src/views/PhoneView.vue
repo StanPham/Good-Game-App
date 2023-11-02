7950517bc2b6281b00788d0f53dcfab3a894b38b
StanPham <stanleypham02@gmail.com>
2023-11-01 19:56:29 -0500

    working phone verification

--- src/views/PhoneView.vue
+++ src/views/PhoneView.vue
@@ -1,14 +1,33 @@
 <script setup>
+import { initializeApp } from "firebase/app";
+import { getAuth, inMemoryPersistence, setPersistence } from 'firebase/auth'
 import { ref } from "vue";
-import { onAuthStateChanged } from "firebase/auth";
-import { firebaseAppAuth } from '@/firebase'
+import { updatePhoneNumber } from "firebase/auth";
+import { onAuthStateChanged, RecaptchaVerifier, PhoneAuthProvider, signInWithCredential } from "firebase/auth";
+import { firebaseAppAuth, firebaseConfig } from '@/firebase'
 const phoneNumber = ref('')
+const verificationCode = ref('')
+const verificationID = ref(null)
+const verification = ref(null)
 const user = ref(null)
 
-const submitPhoneNumber = () => {
+const submitPhoneNumber = async () => {
     console.log(phoneNumber.value)
+    const countryCode = '+1'
+    const applicationVerifier = new RecaptchaVerifier(firebaseAppAuth,'recaptcha-container');
+    const provider = new PhoneAuthProvider(firebaseAppAuth);
+    const fullPhoneNumber = countryCode + phoneNumber.value;
+    verification.value = await provider.verifyPhoneNumber(fullPhoneNumber, applicationVerifier)
+        .then((result) =>{
+            console.log("sms sent")
+            var verifyCode = window.prompt("Please enter the verification \n code that was sent to your device")
+            return PhoneAuthProvider.credential(result,verifyCode)
+        }).then((phoneCred) =>{
+            return updatePhoneNumber(user.value, phoneCred).then((result) => {
+                console.log("worked")
+            })
+        })
 }
-
 onAuthStateChanged(firebaseAppAuth, currentUser => {
     user.value = currentUser
 })
@@ -17,17 +36,19 @@ onAuthStateChanged(firebaseAppAuth, currentUser => {
 <template>
     <div class="phone-main">
         <div v-if="user">
-            {{ user?.email }}
+            email: {{ user?.email }}
+            phone number: {{ user?.phoneNumber }}
         </div>
         <form @submit.prevent="submitPhoneNumber">
             <h1>Phone Number</h1>
             
             <label for="phone">Enter your phone number:</label>
             <input style="color:black;" type="tel" v-model="phoneNumber" name="phone" pattern="[0-9]{10}" required> 
-        
-            <button type="submit" class="submit-btn main-btn">Submit</button>
+            <div id="recaptcha-container"></div>
+            <button type="submit" class="submit-btn main-btn">Send Code</button>
         </form>
     </div>
+
 </template>
 
 <style scoped>
