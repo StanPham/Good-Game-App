54c5abab609b3dcdd97a8888d315cb87df9b1c16
WinnerxSinclair <cobrasuperkek@gmail.com>
2023-11-04 16:10:38 -0500

    maincss update

--- src/components/TableCard.vue
+++ src/components/TableCard.vue
@@ -1,124 +1,68 @@
-
 <template>
-    <div class="container">
-        
-        <div class="event-wrapper">
-            
-           <div class="text-wrapper">
-                <h1 class="event-day">COMING SOON!</h1>
-              
-                
-                    <h2 class="event-title"> </h2>
-           </div>
-                <br><br><br>
-                <button class="main-btn-full" @click="goEvent">coming soon</button>
-                <br> <br> <br><br> <br> <br>
-        </div>
-       
-
+  <div class="main-container rc">
+    <div class="event-wrapper">
+      <div class="text-wrapper">
+        <h1 class="reverse-gradient pad rc-top-top text-center">Reserve a Table</h1>
+      </div>      
     </div>
-    
-
+  </div>
 </template>        
-                
+        
+               
 <style scoped>
 .text-wrapper{
-    position:relative;
-}
-.text-wrapper::before {
-  content: "";
-  position: absolute;
-  
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
- 
-  background: #0706065d;
-  
-filter: blur(10px);
-  z-index: -1;
-}
-
-
-.event-wrapper{
-    padding-top: 1rem;
-    font-size: clamp(1.3vw,1rem, 10vw);
-    text-align: center;
-    
-
-    
-}
-
-
-.container{
-   
-  /* background: linear-gradient(98.79deg, #4517c4 -8.25%, #1d0220 73.07%); */
   position:relative;
-   
-   border-radius: 1rem;
-  width:100%;
-   
-   
-   
-   
-    
-  }
+}
 
-   .container::before{
-    width:100%;
-    content: "";
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-   background-image: url('../images/cardshop.jpg');
-   filter:brightness(35%);
- background-repeat:no-repeat;
- background-size:cover;
+.text-wrapper::before {
+content: "";
+position: absolute;
 
-   z-index:-1;
-     border-radius: 1rem;
-  } 
-    
+top: 0;
+right: 0;
+bottom: 0;
+left: 0;
+
+background: #0706065d;
+
+filter: blur(10px);
+z-index: -1;
+}
+
+.main-container{
+position:relative;
+width:100%;
+height:100%;
+}
+
+.main-container::before{
+width:100%;
+content: "";
+position: absolute;
+top: 0;
+right: 0;
+bottom: 0;
+left: 0;
+background-image: url('../images/cardshop.jpg');
+filter:brightness(35%);
+background-repeat:no-repeat;
+background-size:cover;
+z-index:-1;
+border-radius: 1rem;
+} 
+  
 .event-title{
-    /* padding-left:1rem; */
-    font-weight:inherit;
-    font-style: italic;
-    
-    padding-top:2rem;
-   
-}
-.event-description{
-    padding-top: 5px;
+  font-weight:inherit;
+  font-style: italic;
+  padding-top:2rem;
 }
 
+.event-description{
+  padding-top: 5px;
+}
 
 </style>
 
-<script setup>
-
-import { onMounted, ref, watch, computed } from 'vue'
-
-
-
-
-
-
-
-
-
-
-
-
-
-function goEvent() {
-  this.$router.push('/shop');
-}
-
-
-</script>
 
 
 
