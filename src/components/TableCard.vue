c2169803b274e92137c64f14aa3dcedd9457ddbc
WinnerxSinclair <cobrasuperkek@gmail.com>
2023-11-05 00:17:02 -0500

    table frtonend

--- src/components/TableCard.vue
+++ src/components/TableCard.vue
@@ -1,26 +1,40 @@
+<script setup>
+import router from '../router';
+
+const goTable = () => router.push('/table')
+</script>
+
 <template>
-  <div class="main-container rc gradient">
-    <div class="text-wrapper pad flex column wrap gap text-center">
-      <h1 class="rc-top-top cool-font">Table Reservations</h1>
-      <p class="title">Don't want your spot taken? Reserve a table by clicking the button below.</p>
-      <button class="main-btn-full pad-top">Reserve</button>
+  <div class="main-container rc text-center rel">
+    <div class="header-wrapper gradient rc-top-top">
+      <h1 class=" pad cool-font">Table Reservations</h1>
     </div>
+    <br><br><br><br><br>
+    <button class="main-btn-full" @click="goTable">Reserve Tables</button>
   </div>
 </template>        
         
     
-        
-               
 <style scoped>
-.text-wrapper{
-  align-items:center;
-}
 
+.main-container::before{
+  width:100%;
+  content: "";
+  position: absolute;
+  inset: 0;
+  background-image: url('../images/cat.webp');
+  filter:brightness(35%);
+  background-repeat:no-repeat;
+  background-size:cover;
+  z-index:-1;
+  border-radius: 1rem;
+} 
+
+.header-wrapper{
+  height:20%;
+}
 .main-container{
-width:100%;
-height:100%;
-display:grid;
-grid-template-columns: 1fr 1fr;
+  height:100%;
 }
 
 </style>
