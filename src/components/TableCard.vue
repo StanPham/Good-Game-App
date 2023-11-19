4ae43c8622a18dea1406a8a386055b3be7d53f54
WinnerxSinclair <cobrasuperkek@gmail.com>
2023-11-19 12:17:37 -0600

    tcgplayer card

--- src/components/TableCard.vue
+++ src/components/TableCard.vue
@@ -1,41 +1,44 @@
-<script setup>
-import router from '../router';
-
-const goTable = () => router.push('/table')
-</script>
-
 <template>
-  <div class="main-container rc text-center rel reverse-gradient">
-    <div class="header-wrapper rc-top-top">
-      <h1 class=" pad cool-font">Looking for Singles?</h1>
+  <div class="main-container rc rel">
+    <div class="text-center title-container rel">
+      <h1 class="pad cool-font">Looking for Singles?</h1>
     </div>
-    <br><br><br><br><br>
-    <a class="main-btn-full" href="https://shop.tcgplayer.com/sellerfeedback/71c2420f" target="_blank">TCGplayer</a>
+    
+    <br><br><br><br>
+
+    <div class="text-center rel pad content-wrapper">
+      <div class="title-big italic bold">Visit Our</div>
+      <br>
+      <a class="main-btn-full" href="https://shop.tcgplayer.com/sellerfeedback/71c2420f" target="_blank">TCGplayer</a>
+    </div>
   </div>
 </template>        
         
-    
 <style scoped>
+.main-container{
+  width:100%;
+  height:100%;
+}
 
 .main-container::before{
-  width:50%;
   content: "";
-  position: absolute;
-  inset:75px 0 0 0;
-  background-image: url('../images/testcards.png');
-  background-repeat:no-repeat;
+  position:absolute;
+  inset:0;
+  background-image: url('../images/cards.png');
   background-size:cover;
-  overflow:hidden;
-  transform: scale(1.1);
-  border-radius: 1rem;
-} 
-
-
-.header-wrapper{
-  height:20%;
+  background-repeat:no-repeat;
+  filter:brightness(60%) saturate(1.25);
+  z-index:-5;
+  border-radius:1rem;
 }
-.main-container{
-  height:100%;
+
+.title-container::before{
+  content: "";
+  position:absolute;
+  inset:0;
+  background: linear-gradient(98.79deg, #BA76FF -8.25%, #180030 73.07%);
+  border-radius: 1rem 1rem 0 0;
+  z-index: -2;
 }
 
 </style>
